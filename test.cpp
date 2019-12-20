e33c8932d3ab3e3e14d673dba337a7b6c59af810
JeongHooKwon <kjhh03@gmail.com>
2019-12-20 11:16:51 +0900

    19.12.20 test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,21 @@
+#include <bits/stdc++.h>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-    cout << "Hello" << "\n";
+void solve() {
+    vector<int> v;
+    for (int i = 0; i < 10; i++) {
+        v.push_back(i);
+        cout << i << " ";
+    }
+}
+
+int main(int argc, char *argv[]) {
+   // ios::sync_with_stdio(false); cin.tie(NULL);
+    string c = "code";
+    cout << "Hello" << " " << c << "\n";
+    solve();
+    system("puase");
 }
